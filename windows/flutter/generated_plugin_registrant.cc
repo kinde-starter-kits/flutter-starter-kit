18ad10d0278fe4571c8a49677b00b0c0c4d0ef3d
vu.do <vu.do@saigontechnology.com>
2023-11-13 17:50:18 +0700

    first version

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <kinde_flutter_sdk/kinde_flutter_sdk_plugin_c_api.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  KindeFlutterSdkPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("KindeFlutterSdkPluginCApi"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+}
