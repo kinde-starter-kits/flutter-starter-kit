86a1879bde6ba46052c4dcf5e4c7bb49525e72ff
Victor Eronmosele <victoreronmosele@gmail.com>
2025-10-14 06:38:16 +0100

    Regenerate android folder to use latest APIs and setup

--- android/app/src/main/kotlin/au/kinde/flutter_starter_kit/flutter_starter_kit/MainActivity.kt
+++ android/app/src/main/kotlin/au/kinde/flutter_starter_kit/flutter_starter_kit/MainActivity.kt
@@ -2,5 +2,4 @@ package au.kinde.flutter_starter_kit.flutter_starter_kit
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
